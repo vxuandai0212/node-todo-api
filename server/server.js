b8057f5966cf9822e06608b73fd9b76030ee053b
vxuandai0212 <vxuandai@gmail.com>
2018-04-14 12:23:56 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -21,7 +21,32 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        console.log(todos);
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/todos/:id', (req, res) =>{
+    var id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
+
 app.listen(3000, () => {
     console.log('App running on port 3000');
 });
 
+module.exports = {app};
