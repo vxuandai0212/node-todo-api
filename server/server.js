365a85f6730eab7fd4613f7f1ef15f5e512dc211
vxuandai0212 <vxuandai@gmail.com>
2018-04-15 19:08:08 +0700

    add test DELETE todos/:id route

--- server/server.js
+++ server/server.js
@@ -46,6 +46,21 @@ app.get('/todos/:id', (req, res) =>{
     })
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
+
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
